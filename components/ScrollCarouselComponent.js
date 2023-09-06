4429ca2b8707cffeaee0376f2e5c45b3811ac350
riturajmahapatra <riturajmahapatra@gmail.com>
2023-09-06 11:28:36 +0530

    scrool-comp

--- components/ScrollCarouselComponent.js
+++ components/ScrollCarouselComponent.js
@@ -7,7 +7,7 @@ const ScrollCarouselComponent = () => {
   return (
     <ScrollCarousel autoplay autoplaySpeed={8} speed={7} onReady={() => console.log('I am ready')}>
       {[9, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((item) => (
-        <div key={item} className="h-36 w-48 rounded border-2 border-blue-300/70 bg-blue-300/20">
+        <div key={item} className="h-36 w-48 rounded border-2  border-blue-300/70 bg-blue-300/20">
           {item}
         </div>
       ))}
